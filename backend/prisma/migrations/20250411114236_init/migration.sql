7a85a2cc9dcbc04ba6d2489bc45e20421f5e01a9
yitaikarma <final620472@outlook.com>
2025-04-11 19:50:58 +0800

    chore: 添加migrations到版本控制中，调整ORM生产环境部署流程

---
+++ backend/prisma/migrations/20250411114236_init/migration.sql
@@ -0,0 +1,397 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255) NULL,
+    `email` VARCHAR(255) NULL,
+    `phone` VARCHAR(255) NULL,
+    `password` VARCHAR(255) NULL,
+    `role` TINYINT UNSIGNED NULL DEFAULT 0,
+    `gender` TINYINT NULL DEFAULT 0,
+    `nickname` VARCHAR(255) NULL,
+    `avatar` VARCHAR(255) NULL,
+    `address` VARCHAR(255) NULL,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_username_key`(`username`),
+    INDEX `User_email_idx`(`email`),
+    INDEX `User_username_idx`(`username`),
+    INDEX `User_phone_idx`(`phone`),
+    INDEX `User_role_idx`(`role`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Role` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `value` TINYINT UNSIGNED NOT NULL,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Role_name_key`(`name`),
+    INDEX `Role_name_idx`(`name`),
+    INDEX `Role_value_idx`(`value`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Login_Log` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `browser` TINYINT UNSIGNED NOT NULL,
+    `ip` VARCHAR(255) NOT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Login_Log_email_key`(`email`),
+    INDEX `Login_Log_email_idx`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `System_Log` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `account` VARCHAR(255) NOT NULL,
+    `business_type` VARCHAR(255) NOT NULL,
+    `ip` VARCHAR(255) NOT NULL,
+    `method` VARCHAR(255) NOT NULL,
+    `module` VARCHAR(255) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `System_Log_account_key`(`account`),
+    INDEX `System_Log_account_idx`(`account`),
+    INDEX `System_Log_business_type_idx`(`business_type`),
+    INDEX `System_Log_ip_idx`(`ip`),
+    INDEX `System_Log_module_idx`(`module`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Commodity` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `price` FLOAT NOT NULL DEFAULT 0,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `categoryId` INTEGER UNSIGNED NULL,
+
+    UNIQUE INDEX `Commodity_name_key`(`name`),
+    INDEX `Commodity_name_idx`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CommodityCategory` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `CommodityCategory_name_key`(`name`),
+    INDEX `CommodityCategory_name_idx`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Warehouse` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `province` VARCHAR(255) NOT NULL,
+    `city` VARCHAR(255) NOT NULL,
+    `district` VARCHAR(255) NOT NULL,
+    `address` VARCHAR(255) NULL,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` INTEGER UNSIGNED NULL,
+    `username` VARCHAR(255) NULL,
+    `categoryId` INTEGER UNSIGNED NULL,
+
+    UNIQUE INDEX `Warehouse_name_key`(`name`),
+    INDEX `Warehouse_name_idx`(`name`),
+    INDEX `Warehouse_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `WarehouseCategory` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `WarehouseCategory_name_key`(`name`),
+    INDEX `WarehouseCategory_name_idx`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Inventory` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `total` INTEGER UNSIGNED NOT NULL DEFAULT 0,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `commodityId` INTEGER UNSIGNED NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `InventoryExtension` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `total` INTEGER UNSIGNED NOT NULL DEFAULT 0,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `warehouseId` INTEGER UNSIGNED NULL,
+    `inventoryId` INTEGER UNSIGNED NULL,
+
+    INDEX `InventoryExtension_warehouseId_idx`(`warehouseId`),
+    INDEX `InventoryExtension_inventoryId_idx`(`inventoryId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `InventoryRecord` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `type` TINYINT NOT NULL DEFAULT 0,
+    `total` INTEGER UNSIGNED NOT NULL DEFAULT 0,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `warehouseId` INTEGER UNSIGNED NULL,
+    `commodityId` INTEGER UNSIGNED NULL,
+
+    INDEX `InventoryRecord_type_idx`(`type`),
+    INDEX `InventoryRecord_warehouseId_idx`(`warehouseId`),
+    INDEX `InventoryRecord_commodityId_idx`(`commodityId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Vehicle` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `license` VARCHAR(255) NOT NULL,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` INTEGER UNSIGNED NULL,
+    `username` VARCHAR(255) NULL,
+    `categoryId` INTEGER UNSIGNED NULL,
+
+    UNIQUE INDEX `Vehicle_name_key`(`name`),
+    UNIQUE INDEX `Vehicle_license_key`(`license`),
+    INDEX `Vehicle_name_idx`(`name`),
+    INDEX `Vehicle_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `VehicleCategory` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `VehicleCategory_name_key`(`name`),
+    INDEX `VehicleCategory_name_idx`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Employee` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `gender` TINYINT NOT NULL DEFAULT 0,
+    `id_card` VARCHAR(255) NOT NULL,
+    `phone` VARCHAR(255) NULL,
+    `address` VARCHAR(255) NULL,
+    `desc` TEXT NULL,
+    `department` TINYINT NOT NULL DEFAULT 0,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Employee_id_card_key`(`id_card`),
+    UNIQUE INDEX `Employee_phone_key`(`phone`),
+    INDEX `Employee_name_idx`(`name`),
+    UNIQUE INDEX `Employee_id_name_key`(`id`, `name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Order` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `fromProvince` VARCHAR(255) NULL,
+    `fromCity` VARCHAR(255) NULL,
+    `fromDistrict` VARCHAR(255) NULL,
+    `fromAddress` VARCHAR(255) NULL,
+    `toProvince` VARCHAR(255) NOT NULL,
+    `toCity` VARCHAR(255) NOT NULL,
+    `toDistrict` VARCHAR(255) NOT NULL,
+    `toAddress` VARCHAR(255) NULL,
+    `receiver` VARCHAR(255) NOT NULL,
+    `phone` VARCHAR(255) NOT NULL,
+    `total` INTEGER UNSIGNED NOT NULL DEFAULT 0,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `inventoryId` INTEGER UNSIGNED NULL,
+    `warehouseId` INTEGER UNSIGNED NULL,
+    `userId` INTEGER UNSIGNED NULL,
+    `categoryId` INTEGER UNSIGNED NULL,
+
+    INDEX `Order_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `OrderCategory` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `OrderCategory_name_key`(`name`),
+    INDEX `OrderCategory_name_idx`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Task` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `desc` TEXT NULL,
+    `startTime` DATETIME(3) NULL,
+    `endTime` DATETIME(3) NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` INTEGER UNSIGNED NULL,
+    `username` VARCHAR(255) NULL,
+    `categoryId` INTEGER UNSIGNED NULL,
+
+    UNIQUE INDEX `Task_name_key`(`name`),
+    INDEX `Task_name_idx`(`name`),
+    INDEX `Task_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `TaskCategory` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `TaskCategory_name_key`(`name`),
+    INDEX `TaskCategory_name_idx`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Distribution` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `categoryId` INTEGER UNSIGNED NULL,
+
+    UNIQUE INDEX `Distribution_name_key`(`name`),
+    INDEX `Distribution_name_idx`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `DistributionCategory` (
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `desc` TEXT NULL,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `DistributionCategory_name_key`(`name`),
+    INDEX `DistributionCategory_name_idx`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Commodity` ADD CONSTRAINT `Commodity_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `CommodityCategory`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Warehouse` ADD CONSTRAINT `Warehouse_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Warehouse` ADD CONSTRAINT `Warehouse_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `WarehouseCategory`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Inventory` ADD CONSTRAINT `Inventory_commodityId_fkey` FOREIGN KEY (`commodityId`) REFERENCES `Commodity`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `InventoryExtension` ADD CONSTRAINT `InventoryExtension_warehouseId_fkey` FOREIGN KEY (`warehouseId`) REFERENCES `Warehouse`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `InventoryExtension` ADD CONSTRAINT `InventoryExtension_inventoryId_fkey` FOREIGN KEY (`inventoryId`) REFERENCES `Inventory`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `InventoryRecord` ADD CONSTRAINT `InventoryRecord_warehouseId_fkey` FOREIGN KEY (`warehouseId`) REFERENCES `Warehouse`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `InventoryRecord` ADD CONSTRAINT `InventoryRecord_commodityId_fkey` FOREIGN KEY (`commodityId`) REFERENCES `Commodity`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Vehicle` ADD CONSTRAINT `Vehicle_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Vehicle` ADD CONSTRAINT `Vehicle_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `VehicleCategory`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Order` ADD CONSTRAINT `Order_inventoryId_fkey` FOREIGN KEY (`inventoryId`) REFERENCES `Inventory`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Order` ADD CONSTRAINT `Order_warehouseId_fkey` FOREIGN KEY (`warehouseId`) REFERENCES `Warehouse`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Order` ADD CONSTRAINT `Order_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Order` ADD CONSTRAINT `Order_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `OrderCategory`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Task` ADD CONSTRAINT `Task_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Task` ADD CONSTRAINT `Task_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `TaskCategory`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Distribution` ADD CONSTRAINT `Distribution_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `DistributionCategory`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
